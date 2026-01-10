e2dd937c93a5d4a005204bdbca5e728bf515c384
Luke <lukesmith12601@gmail.com>
2026-01-10 00:23:50 -0500

    fixes

--- fs_dev2_team_Deepest/Assets/Scripts/InventorySlot.cs
+++ fs_dev2_team_Deepest/Assets/Scripts/InventorySlot.cs
@@ -1,9 +1,9 @@
-using UnityEngine.EventSystems;
+﻿using UnityEngine.EventSystems;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class InventorySlot : MonoBehaviour, ISubmitHandler, IPointerClickHandler, ISelectHandler, IPointerEnterHandler, IPointerExitHandler
+public class InventorySlot : MonoBehaviour, ISubmitHandler, IPointerClickHandler, ISelectHandler, IPointerEnterHandler, IPointerExitHandler, IDeselectHandler
 {
     public ItemData itemInSlot;
     public GameObject slotPref;
@@ -30,56 +30,39 @@ public class InventorySlot : MonoBehaviour, ISubmitHandler, IPointerClickHandler
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        if (eventData.button == PointerEventData.InputButton.Left)
-        {
-            if(InventoryManager.instance.selectedSlot != this)
-            {
-                InventoryManager.instance.selectedSlot = this;
-                switch (itemInSlot.itemType)
-                {
-                    case ItemType.Weapon:
-                        InventoryManager.instance.weaponImage.sprite = itemInSlot.itemIcon;
-                        break;
-                    case ItemType.ChestPiece:
-                        InventoryManager.instance.chestPieceImage.sprite = itemInSlot.itemIcon;
-                        break;
-                    case ItemType.Leggings:
-                        InventoryManager.instance.leggingsPieceImage.sprite = itemInSlot.itemIcon;
-                        break;
-                    case ItemType.Gauntlets:
-                        InventoryManager.instance.leftGauntletPieceImage.sprite = itemInSlot.itemIcon;
-                        InventoryManager.instance.rightGauntletPieceImage.sprite = itemInSlot.itemIcon;
-                        break;
-                    case ItemType.Ring:
-                        InventoryManager.instance.ringImage.sprite = itemInSlot.itemIcon;
-                        break;
-                }
-            }
-            else
-            {
-                InventoryManager.instance.selectedSlot = null;
-                switch (itemInSlot.itemType)
-                {
-                    case ItemType.Weapon:
-                        InventoryManager.instance.weaponImage.sprite = null;
-                        break;
-                    case ItemType.ChestPiece:
-                        InventoryManager.instance.chestPieceImage.sprite = null;
-                        break;
-                    case ItemType.Leggings:
-                        InventoryManager.instance.leggingsPieceImage.sprite = null;
-                        break;
-                    case ItemType.Gauntlets:
-                        InventoryManager.instance.leftGauntletPieceImage.sprite = null;
-                        InventoryManager.instance.rightGauntletPieceImage.sprite = null;
-                        break;
-                    case ItemType.Ring:
-                        InventoryManager.instance.ringImage.sprite = null;
-                        break;
-                }
-            }
+        if (eventData.button != PointerEventData.InputButton.Left)
+            return;
 
-            
+        // Clicking the same slot again → deselect
+        if (InventoryManager.instance.selectedSlot == this)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            return;
+        }
+
+        // Otherwise select this slot
+        if (InventoryManager.instance.selectedSlot != this)
+        {
+            InventoryManager.instance.selectedSlot = this;
+            switch (itemInSlot.itemType)
+            {
+                case ItemType.Weapon:
+                    InventoryManager.instance.weaponImage.sprite = itemInSlot.itemIcon;
+                    break;
+                case ItemType.ChestPiece:
+                    InventoryManager.instance.chestPieceImage.sprite = itemInSlot.itemIcon;
+                    break;
+                case ItemType.Leggings:
+                    InventoryManager.instance.leggingsPieceImage.sprite = itemInSlot.itemIcon;
+                    break;
+                case ItemType.Gauntlets:
+                    InventoryManager.instance.leftGauntletPieceImage.sprite = itemInSlot.itemIcon;
+                    InventoryManager.instance.rightGauntletPieceImage.sprite = itemInSlot.itemIcon;
+                    break;
+                case ItemType.Ring:
+                    InventoryManager.instance.ringImage.sprite = itemInSlot.itemIcon;
+                    break;
+            }
         }
     }
 
@@ -90,7 +73,8 @@ public class InventorySlot : MonoBehaviour, ISubmitHandler, IPointerClickHandler
 
     public void OnSelect(BaseEventData eventData)
     {
-
+        
+       
     }
 
     public void OnSubmit(BaseEventData eventData)
@@ -116,5 +100,31 @@ public class InventorySlot : MonoBehaviour, ISubmitHandler, IPointerClickHandler
             InventoryManager.instance.itemImage.sprite = null;
             InventoryManager.instance.itemDescriptionBox.text = null;
         }
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        InventoryManager.instance.selectedSlot = null;
+        switch (itemInSlot.itemType)
+        {
+            case ItemType.Weapon:
+                InventoryManager.instance.weaponImage.sprite = null;
+                break;
+            case ItemType.ChestPiece:
+                InventoryManager.instance.chestPieceImage.sprite = null;
+                break;
+            case ItemType.Leggings:
+                InventoryManager.instance.leggingsPieceImage.sprite = null;
+                break;
+            case ItemType.Gauntlets:
+                InventoryManager.instance.leftGauntletPieceImage.sprite = null;
+                InventoryManager.instance.rightGauntletPieceImage.sprite = null;
+                break;
+            case ItemType.Ring:
+                InventoryManager.instance.ringImage.sprite = null;
+                break;
+        }
+        InventoryManager.instance.itemImage.sprite = null;
+        InventoryManager.instance.itemDescriptionBox.text = null;
     }
 }
--- fs_dev2_team_Deepest/Assets/Scripts/UImanager.cs
+++ fs_dev2_team_Deepest/Assets/Scripts/UImanager.cs
@@ -9,7 +9,7 @@ public class UImanager : MonoBehaviour
     public GameObject playerDamageScreen;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         instance = this;
     }
